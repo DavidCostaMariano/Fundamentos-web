24febc04d53a9e9c5325df5cb3a88579ee22b919
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-06-18 12:39:53 -0300

    implementando o javascript

---
+++ assets/Js/script.js
@@ -0,0 +1,15 @@
+function scrollGostos()
+{
+    window.scrollTo(0 , document.querySelector('#meus-gostos').offsetTop);
+   
+}
+function scrollVideo()
+{
+    window.scrollTo(0 , document.querySelector('#video').offsetTop);
+   
+}
+function scrollNome()
+{
+    window.scrollTo(0 , document.querySelector('#nome').offsetTop);
+   
+}
